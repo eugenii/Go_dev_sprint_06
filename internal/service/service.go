a730b4db478be7b78147be08b41a0e5d3b10d95c
eugenii <eugenii.cher@gmail.com>
2025-12-05 10:26:22 +0300

    begining

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"pkg/morse"
+)
+
+const text = "АБВГДЕЁЖЗИЙКЛМНОПРССТУФХЦЧШЩЪЫЬЭЮЯ!1234567890)(,:;%@\\|/"
+
+func Decode(input string) string {
+	input = strings.ToUpper(input)
+
+	if len(input) == 0 {
+		fmt.Println("input is empty")
+		return ""
+	}
+
+	if strings.ContainsAny(input, text) {
+		return morse.ToMorse(input)
+	} else {
+		return morse.ToText(input)
+	}
+
+}
